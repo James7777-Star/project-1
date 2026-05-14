1a3a84666b0c2e0e3bb5a75e62d060f6fb1b079d
User <qitiand450@gmail.com>
2026-05-14 17:48:42 +0800

    Extra points questions

--- Extra points questions.py
+++ Extra points questions.py
@@ -1,61 +1,62 @@
 import numpy as np
 import csv
 
-input_file = "Electronics_Unique_10000.csv"
-output_file = "Electronics_Final_Report.csv"
+products = []
+price = []
+electronics = []
+sales = []
 
-products, price, stock, sales = [], [], [], []
-
-with open(input_file, "r", encoding="utf-8-sig") as f:
+with open("Electronics_Unique_10000.csv", "r", encoding="utf-8-sig") as f:
     reader = csv.DictReader(f)
     for row in reader:
         products.append(row["Product_Name"])
         p = float(row["Unit_Price"].replace('$', '').replace(',', '').strip())
         price.append(p)
-        stock.append(float(row["Stock_Quantity"]))
+        electronics.append(float(row["Stock_Quantity"]))
         sales.append(float(row["Sales_Volume"]))
 
 products = np.array(products)
 price = np.array(price)
-stock = np.array(stock)
+electronics = np.array(electronics)
 sales = np.array(sales)
 
-
-total_value = price * stock
+total_electronics_value = price * electronics
 discount_revenue = sales * price * 0.8
+
 
 sorted_indices = np.argsort(sales)
 hot_3_idx = sorted_indices[-3:][::-1]
 low_3_idx = sorted_indices[:3]
 
-print("\n" + "="*70)
-print(f" 【最暢銷前三名】")
+
+print(f"{'排名':<4} | {'銷售量':<6} | {'產品名稱'}")
+print("-" * 60)
+print(" 【最暢銷前三名】")
 for i, idx in enumerate(hot_3_idx, 1):
-    print(f"第 {i} 名: {products[idx]} (銷量: {sales[idx]:.0f})")
+    print(f"第 {i} 名 | {sales[idx]:>6.0f} | {products[idx]}")
 
-print("-" * 70)
-print(f"❄ 【最不暢銷前三名】")
+print("-" * 60)
+print(" 【最不暢銷前三名】")
 for i, idx in enumerate(low_3_idx, 1):
-    print(f"第 {i} 名: {products[idx]} (銷量: {sales[idx]:.0f})")
+    print(f"第 {i} 名 | {sales[idx]:>6.0f} | {products[idx]}")
 
-print("-" * 70)
+print("-" * 60)
+print(f" 全部商品八折總收入為：${np.sum(discount_revenue):,.2f}")
+print("-" * 60)
 
-print(f" 全部商品八折後的總收入為：${np.sum(discount_revenue):,.2f}")
-print("="*70 + "\n")
 
-
-with open(output_file, "w", newline="", encoding="utf-8-sig") as f:
+with open("Electronics_Analysis.csv", "w", newline="", encoding="utf-8-sig") as f:
     w = csv.writer(f)
 
-    w.writerow(["Product", "Stock", "Price", "Sales", "Inventory_Value"])
+    w.writerow(["Product_Name", "Electronics_Stock", "Unit_Price", "Sales_Volume", "Inventory_Value"])
 
     for i in range(len(products)):
         w.writerow([
             products[i],
-            stock[i],
+            electronics[i],
             price[i],
             sales[i],
-            total_value[i]
+            total_electronics_value[i]
         ])
 
-print(f" 分析完畢！CSV 檔案已儲存為：{output_file}")
+print(" Electronics_Analysis.csv 已存檔完成。")
